ad1016b8ab0fe1e1221ea08e9ac809a59d99885d
Adrian Peterson Co <adrianpco@gmail.com>
2015-03-22 10:56:17 +0800

    Adds Mandrill for mailer sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.mandrillapp.com",
+    :port           => "587",
+    :authentication => :plain,
+    :user_name      => Rails.application.secrets.mandrill_username,
+    :password       => Rails.application.secrets.mandrill_password,
+    :domain         => "heroku.com",
+    :enable_starttls_auto => true
+  }
+
 end
