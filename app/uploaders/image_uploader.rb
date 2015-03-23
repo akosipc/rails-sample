ec593929b35bbeef50b899ddb760b3130f5a0b30
Adrian Peterson Co <adrianpco@gmail.com>
2015-03-24 03:49:18 +0800

    Lets try this in staging

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-
-  if Rails.env.production?
-    include Cloudinary::CarrierWave
-  end
-
-  storage :file
+  include Cloudinary::CarrierWave
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
