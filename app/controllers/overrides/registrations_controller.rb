c116951bbef51084076848e148e17041d1bff5de
Adrian Peterson Co <adrianpco@gmail.com>
2015-03-31 01:29:39 +0800

    Fixes after_sign_in_path_for

--- app/controllers/overrides/registrations_controller.rb
+++ app/controllers/overrides/registrations_controller.rb
@@ -1,8 +1,13 @@
 class Overrides::RegistrationsController < Devise::RegistrationsController
-  protected
-    def sign_up_params
-      params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
-    end
+
+protected
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
+  end
+
+  def after_sign_up_path_for(resource)
+    new_question_group_answer_groups_path(Rapidfire::Question.first)
+  end
 end
 
 
